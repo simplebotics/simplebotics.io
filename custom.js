5a77b9fe0ff62e661d4d773485e0910df8e4ea24
simplebotics <simpleboticsblog@gmail.com>
2016-10-05 13:03:06 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -254,7 +254,7 @@
 
   $(function(){
       $(".typed-header").typed({
-        strings: ["robotics", "drones", "apps", "electronics", "coding", "mechanics"],
+        strings: ["robotics", "drones", "apps", "electronics", "coding"],
         typeSpeed: 90,
         loop: true,
         backDelay: 3000,
